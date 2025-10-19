82b11bf051af17623832eea16a3ccbcf2923b523
Lefort14 <maxibon2004@gmail.com>
2025-10-19 17:31:52 +0300

    добавлена команда /playlist, отображающая список треков в папке

--- commands/utility/controllers/get-music-data.js
+++ commands/utility/controllers/get-music-data.js
@@ -5,11 +5,13 @@ const NodeID3 = require("node-id3");
 const MUSIC_PATH = require("./path.js");
 const path = require("path");
 const mm = require("music-metadata");
+const { playNav } = require("../classes/pageNavigator.js");
 
 module.exports = {
-    getMP3Metadata,
-    embedFn
-}
+  getMP3Metadata,
+  embedFn,
+  playList,
+};
 
 async function getMP3Metadata(track, interaction) {
   const files = fs.readdirSync(MUSIC_PATH); // перебираем папку
@@ -18,6 +20,11 @@ async function getMP3Metadata(track, interaction) {
     // фильтруем на название трека в нижнем регистре
     return file.toLowerCase().includes(track.toLowerCase());
   });
+
+  if (!founder[0].toLowerCase().endsWith('.mp3')) {
+  await interaction.editReply('**Найденный файл не в .mp3 формате!**');
+  return false;
+  }
 
   if (founder.length === 0) {
     interaction.editReply(`**Трек "${track}" не найден!**`);
@@ -62,7 +69,7 @@ async function getMP3Metadata(track, interaction) {
 
 // * ///////////////////////
 
-async function embedFn(songName, authorName, time, url, files, interaction) {
+async function embedFn(songName, authorName, time, url, files) {
   return {
     embeds: [
       new EmbedBuilder()
@@ -118,5 +125,48 @@ async function getTrackDuration(filePath) {
       error.message
     );
     return "00:00";
+  }
+}
+
+// * /////////////////
+
+async function playList(interaction) {
+  try {
+    console.log(`Запрос списка треков`);
+
+    const files = fs.readdirSync(MUSIC_PATH).filter(file => file.toLowerCase().endsWith('.mp3'))
+    const pages = [];
+    const num = 20;
+
+    for (let i = 0; i < files.length; i += num) {
+      const slice = files.slice(i, i + num);
+
+      const page = slice
+        .map((track) => {
+          return `${track.replace(/\.mp3$/i, "")}`;
+        })
+        .join("\n\n");
+
+      pages.push(page);
+    }
+
+    playNav.setPages(pages);
+
+    playNav.embed
+      .setDescription(playNav.current())
+      .setTitle(`**Список песен:**`);
+
+    playNav.pageRow.components[0].setCustomId("playList_prev_page");
+    playNav.pageRow.components[1].setCustomId("playList_next_page");
+
+    await interaction.editReply({
+      embeds: [playNav.embed],
+      components: [playNav.pageRow],
+    });
+  } catch (error) {
+    console.log(`Произошла ошибка при выводе очереди: ${error.message}`);
+    return interaction.editReply(
+      `**Произошла ошибка при выводе листа треков!**`
+    );
   }
 }
